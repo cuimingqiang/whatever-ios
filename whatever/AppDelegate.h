a5ca5fc142823c088263f3683c9e51b94d45fcbe
cuimingqiang <admin@cuimingqiang-iMac.lan>
2016-05-29 23:29:11 +0800

    init commit

---
+++ whatever/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatever
+//
+//  Created by admin on 16/5/29.
+//  Copyright © 2016年 cmq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
